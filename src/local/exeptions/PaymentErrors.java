73bd49cab3faa0aca713c756cdb7672ab096be47
Ivan <semen_gen@tut.by>
2020-05-31 15:25:12 +0300

    Создал исключение для ошибок оплаты

---
+++ src/local/exeptions/PaymentErrors.java
@@ -0,0 +1,14 @@
+package local.exeptions;
+
+public class PaymentErrors extends Throwable {
+
+    public PaymentErrors(String s) {
+        super(s);
+    }
+
+    public void printMessage() {
+        System.out.println("Проблемы при оплате:");
+        System.out.println(this.getMessage());
+    }
+
+}
